a9a547ec61235b398d929ccb0a33da95676c9939
Cawe Coy Rodrigues Marega <programacao@innbativel.com.br>
2014-02-19 11:20:49 -0300

    Empresas: errinho corrigido

--- app/database/seeds/ProfilesTableSeeder.php
+++ app/database/seeds/ProfilesTableSeeder.php
@@ -1,29 +1,29 @@
 <?php
 
-use Goodby\CSV\Import\Standard\Lexer;
-use Goodby\CSV\Import\Standard\Interpreter;
-use Goodby\CSV\Import\Standard\LexerConfig;
+// use Goodby\CSV\Import\Standard\Lexer;
+// use Goodby\CSV\Import\Standard\Interpreter;
+// use Goodby\CSV\Import\Standard\LexerConfig;
 
-class ProfilesTableSeeder extends DatabaseSeeder
-{
-  public function run()
-  {
-    $pdo = new PDO('mysql:host=innbativel.ctypvw54pblq.us-east-1.rds.amazonaws.com;dbname=innbativel', 'innbativel', 'aWdh2kHAF6A3');
+// class ProfilesTableSeeder extends DatabaseSeeder
+// {
+//   public function run()
+//   {
+//     $pdo = new PDO('mysql:host=innbativel.ctypvw54pblq.us-east-1.rds.amazonaws.com;dbname=innbativel', 'innbativel', 'aWdh2kHAF6A3');
 
-	$config = new LexerConfig();
-	$config->setToCharset('UTF-8');
-	$lexer = new Lexer($config); //ISO8591 ou UTF-8
+// 	$config = new LexerConfig();
+// 	$config->setToCharset('UTF-8');
+// 	$lexer = new Lexer($config); //ISO8591 ou UTF-8
 
-	$interpreter = new Interpreter();
+// 	$interpreter = new Interpreter();
 
-	$interpreter->addObserver(function(array $columns) use ($pdo) {
-		$stmt = $pdo->prepare('INSERT INTO profiles (id, user_id, facebook_id, first_name, last_name, birthday, cpf, telephone, telephone2, img, credit, city, state, street, number, complement, neighborhood, zip, company_name, cnpj, site, coordinates, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-	    $stmt->execute($columns);
-	});
+// 	$interpreter->addObserver(function(array $columns) use ($pdo) {
+// 		$stmt = $pdo->prepare('INSERT INTO profiles (id, user_id, facebook_id, first_name, last_name, birthday, cpf, telephone, telephone2, img, credit, city, state, street, number, complement, neighborhood, zip, company_name, cnpj, site, coordinates, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+// 	    $stmt->execute($columns);
+// 	});
 
-	$lexer->parse(app_path().'/database/seeds/assets/profiles.csv', $interpreter);
-  }
-}
+// 	$lexer->parse(app_path().'/database/seeds/assets/profiles.csv', $interpreter);
+//   }
+// }
 
 class ProfilesTableSeeder extends DatabaseSeeder
 {
