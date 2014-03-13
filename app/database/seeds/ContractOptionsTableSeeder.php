7d03f74c4e93b535ee83cec8077552a40ace2bb0
Cawe Coy Rodrigues Marega <programacao@innbativel.com.br>
2014-03-13 17:42:02 -0300

    Incluindo ofertas de exemplo para implementação da home

--- app/database/seeds/ContractOptionsTableSeeder.php
+++ app/database/seeds/ContractOptionsTableSeeder.php
@@ -8,20 +8,20 @@ class ContractOptionsTableSeeder extends DatabaseSeeder
 {
   public function run()
   {
-    $pdo = new PDO('mysql:host=innbativel.ctypvw54pblq.us-east-1.rds.amazonaws.com;dbname=innbativel', 'innbativel', 'aWdh2kHAF6A3');
+	// $pdo = new PDO('mysql:host=innbativel.ctypvw54pblq.us-east-1.rds.amazonaws.com;dbname=innbativel', 'innbativel', 'aWdh2kHAF6A3');
 
-    $config = new LexerConfig();
-    $config->setToCharset('UTF-8')->setDelimiter(';');
-    $lexer = new Lexer($config); //ISO8591 ou UTF-8
+	// $config = new LexerConfig();
+	// $config->setToCharset('UTF-8')->setDelimiter(';');
+	// $lexer = new Lexer($config); //ISO8591 ou UTF-8
 
-    $interpreter = new Interpreter();
+	// $interpreter = new Interpreter();
 
-    $interpreter->addObserver(function(array $columns) use ($pdo) {
-        $stmt = $pdo->prepare('INSERT INTO contract_options (id, contract_id, description, price_original, price_with_discount, percent_off, transfer, max_qty) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
-        $stmt->execute($columns);
-    });
+	// $interpreter->addObserver(function(array $columns) use ($pdo) {
+	//     $stmt = $pdo->prepare('INSERT INTO contract_options (id, contract_id, description, price_original, price_with_discount, percent_off, transfer, max_qty) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+	//     $stmt->execute($columns);
+	// });
 
-    $lexer->parse(app_path().'/database/seeds/assets/contract_options.csv', $interpreter);
+	// $lexer->parse(app_path().'/database/seeds/assets/contract_options.csv', $interpreter);
   }
 }
 
--- app/database/seeds/ContractsTableSeeder.php
+++ app/database/seeds/ContractsTableSeeder.php
@@ -8,20 +8,20 @@ class ContractsTableSeeder extends DatabaseSeeder
 {
   public function run()
   {
-    $pdo = new PDO('mysql:host=innbativel.ctypvw54pblq.us-east-1.rds.amazonaws.com;dbname=innbativel', 'innbativel', 'aWdh2kHAF6A3');
+	// $pdo = new PDO('mysql:host=innbativel.ctypvw54pblq.us-east-1.rds.amazonaws.com;dbname=innbativel', 'innbativel', 'aWdh2kHAF6A3');
 
-    $config = new LexerConfig();
-    $config->setToCharset('UTF-8')->setDelimiter(';');
-    $lexer = new Lexer($config); //ISO8591 ou UTF-8
+	// $config = new LexerConfig();
+	// $config->setToCharset('UTF-8')->setDelimiter(';');
+	// $lexer = new Lexer($config); //ISO8591 ou UTF-8
 
-    $interpreter = new Interpreter();
+	// $interpreter = new Interpreter();
 
-    $interpreter->addObserver(function(array $columns) use ($pdo) {
-    	$stmt = $pdo->prepare('INSERT INTO contracts (id, partner_id, company_name, cnpj, trading_name, address, complement, neighborhood, zip, city, state, agent1_name, agent1_cpf, agent1_telephone, agent2_name, agent2_cpf, agent2_telephone, bank_name, bank_number, bank_holder, bank_agency, bank_account, bank_financial_email, is_signed, is_sent, consultant, term, restriction, has_scheduling, sched_contact, sched_max_date, sched_dates, sched_min_antecedence, n_people, details, clauses, ip, signed_at, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-        $stmt->execute($columns);
-    });
+	// $interpreter->addObserver(function(array $columns) use ($pdo) {
+	// 	$stmt = $pdo->prepare('INSERT INTO contracts (id, partner_id, company_name, cnpj, trading_name, address, complement, neighborhood, zip, city, state, agent1_name, agent1_cpf, agent1_telephone, agent2_name, agent2_cpf, agent2_telephone, bank_name, bank_number, bank_holder, bank_agency, bank_account, bank_financial_email, is_signed, is_sent, consultant, term, restriction, has_scheduling, sched_contact, sched_max_date, sched_dates, sched_min_antecedence, n_people, details, clauses, ip, signed_at, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+	//     $stmt->execute($columns);
+	// });
 
-    $lexer->parse(app_path().'/database/seeds/assets/contracts.csv', $interpreter);
+	// $lexer->parse(app_path().'/database/seeds/assets/contracts.csv', $interpreter);
   }
 }
 
