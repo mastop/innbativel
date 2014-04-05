6736274e110fe966e884bbe3ab9ba78371911058
fernanDOTdo <o@fernan.do>
2014-04-05 05:23:38 -0300

    Mais trabalho na tela de criação da oferta

--- app/views/admin/offer/create.blade.php
+++ app/views/admin/offer/create.blade.php
@@ -10,17 +10,18 @@
         {{ Former::text('subtitle', 'Subtítulo')->class('span12') }}
         {{ Former::text('subsubtitle', 'Subtítulo 2')->class('span12') }}
         {{ Former::text('saveme_title', 'Título SaveMe')->class('span12') }}
-        {{ Former::select('destiny_id', 'Destino')->fromQuery(Destiny::all(), 'name')->class('span12') }}
+        {{ Former::select('destiny_id', 'Destino')->fromQuery(Destiny::all(), 'name')->class('span12  chosen-select') }}
 
         {{ Former::select('partner_id', 'Empresa Parceira')
-        ->addOption('-- SELECIONE --', null)
+        ->addOption(null)
+        ->data_placeholder('Selecione uma Empresa Parceira')
         ->fromQuery(User::getAllByRole('parceiro'))
-        ->class('span12') }}
+        ->class('span12 chosen-select') }}
 
-        {{ Former::select('ngo_id', 'Ong para Doação')
-        ->addOption('-- SELECIONE --', null)
+        {{ Former::select('ngo_id', 'ONG para Doação')
+        ->data_placeholder('Selecione uma ONG')
         ->fromQuery(Ngo::orderBy('name')->get(), 'name')
-        ->class('span12')
+        ->class('span12 chosen-select')
         }}
 
         {{ Former::date('starts_on', 'Início da Oferta')->class('span12') }}
@@ -31,12 +32,25 @@
         {{ Former::textarea('rules', 'Regras')->rows(10)->columns(20)->class('span12') }}
 
 
+        {{ Former::multiselect('offers_includes', 'Inclusos')
+        ->fromQuery(Included::orderBy('title')->get())
+        ->data_placeholder('Selecione os Itens Inclusos')
+        ->class('span12  chosen-select') }}
+
+        {{ Former::select('tell_us_id', 'Depoimento de Cliente')
+        ->addOption(null)
+        ->fromQuery(TellUs::orderBy('destiny')->get())
+        ->data_placeholder('Selecione um Depoimento')
+        ->class('span12 chosen-select') }}
 
         {{ Former::actions()
           ->primary_submit('Criar Oferta')
           ->inverse_reset('Limpar') }}
 
         {{ Former::close() }}
+
+
+        <script src="{{ asset('assets/themes/floripa/backend/js/offer.create.js') }}"></script>
 
     </div>
 
