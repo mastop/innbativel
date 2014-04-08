870ddd176df38f2bfa489edfdd34de982c5ab368
salima <saulolimajf@gmail.com>
2014-04-08 00:05:44 -0300

    criado o controlador para o dashboard do usuário

---
+++ app/controllers/PainelController.php
@@ -0,0 +1,16 @@
+<?php
+
+class PainelController extends BaseController {
+
+    public function __construct()
+    {
+        $this->sidebar = true;
+    }
+
+    public function getDashboard()
+    {
+        $this->layout->content = View::make('painel.dash.index');
+    }
+
+}
+
