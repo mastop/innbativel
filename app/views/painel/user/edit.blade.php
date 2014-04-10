195dc919c48459b86696ada5b23f4e4f457e8f47
salima <saulolimajf@gmail.com>
2014-04-09 21:11:45 -0300

    view da edição do usuário

---
+++ app/views/painel/user/edit.blade.php
@@ -0,0 +1,44 @@
+@section('content')
+
+<div class="well widget row-fluid">
+
+    {{ Former::horizontal_open()->rules([
+            'email' => 'required|email',
+            'profile[first_name]' => 'required',
+            'profile[last_name]' => 'required',
+            'profile[cpf]' => 'required',
+            'profile[city]' => 'required',
+            'profile[state]' => 'required|exists:states,id',
+            'profile[country]' => 'required',
+        ])
+    }}
+
+    {{ Former::text('profile[first_name]', 'Nome')->class('span12') }}
+    {{ Former::text('profile[last_name]', 'Sobrenome')->class('span12') }}
+    {{ Former::text('profile[city]', 'Cidade')->class('span12') }}
+    {{ Former::select('profile[state]', 'Estado')
+        ->addOption('-- selecione uma opção --', null)
+        ->fromQuery(DB::table('states')->get(['id', 'name']), 'name', 'id')
+        ->class('span12')
+    }}
+    {{ Former::text('profile[country]', 'País')->class('span12') }}
+    {{ Former::text('profile[telephone]', 'Telefone')->class('span12') }}
+    {{ Former::text('profile[cpf]', 'CPF')->class('span12') }}
+    {{ Former::text('profile[birthday]', 'Nascimento')->class('span12') }}
+    {{ Former::text('profile[street]', 'Endereço')->class('span12') }}
+    {{ Former::text('profile[number]', 'Número')->class('span12') }}
+    {{ Former::text('profile[neighborhood]', 'Bairro')->class('span12') }}
+    {{ Former::text('profile[zip]', 'CEP')->class('span12') }}
+    {{ Former::text('profile[complement]', 'Complemento')->class('span12') }}
+
+    {{ Former::hidden('username') }}
+    {{ Former::hidden('email') }}
+
+    {{ Former::actions()
+        ->primary_submit('Enviar')
+        ->inverse_reset('Limpar') }}
+    {{ Former::close() }}
+
+</div>
+
+@stop
