571f13f55857cda545d95419cb449f660f36f00a
salima <saulolimajf@gmail.com>
2014-04-09 21:18:03 -0300

    retirada das rotas do painel do usuário

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -469,31 +469,5 @@ Route::group(array('prefix' => 'admin', 'before' => 'auth|perm'), function(){
 	Route::any('contract/delete', function(){ return Redirect::route('admin.contract'); });
 	Route::get('contract/delete/{id}', ['as' => 'admin.contract.delete', 'uses' => 'AdminContractController@getDelete']);
 	Route::post('contract/delete/{id}', ['as' => 'admin.contract.destroy', 'uses' => 'AdminContractController@postDelete']);
-
 });
-
-
-Route::group(array('prefix' => 'painel', 'before' => 'auth'), function(){
-    /*
-	 * Painel/Dashboard
-	 */
-    Route::any('/', ['as' => 'painel', 'uses' => 'PainelController@getDashboard', 'after' => 'cache.public']);
-
-    /*
-     * Painel/Orders
-     */
-	Route::any('ofertas', ['as' => 'painel.order.offers', 'uses' => 'PainelOrderController@anyListByOffer']);
-
-	Route::any('ofertas/exportar', function(){ return Redirect::route('painel.order.offers'); });
-	Route::get('ofertas/exportar/{offer_id?}/{starts_on?}/{ends_on?}', ['as' => 'painel.order.list_offers_export', 'uses' => 'PainelOrderController@getListOffersExport']);
-
-	Route::any('ofertas/voucher/{offer_option_id?}', ['as' => 'painel.order.voucher', 'uses' => 'PainelOrderController@anyVouchers']);
-
-	Route::any('ofertas/voucher/agendar', function(){ return Redirect::route('painel.order.offers'); });
-	Route::get('ofertas/voucher/agendar/{id}/{used}/{offer_option_id?}', ['as' => 'painel.order.schedule', 'uses' => 'PainelOrderController@getSchedule']);
-
-	Route::any('ofertas/voucher/exportar', function(){ return Redirect::route('painel.order.offers'); });
-	Route::get('ofertas/voucher/exportar/{offer_option_id?}/{id?}', ['as' => 'painel.order.voucher_export', 'uses' => 'PainelOrderController@getVoucherExport']);
-});
-
 
