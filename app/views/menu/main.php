da31539fdf2baf33dc683c07d088ce6444396439
salima <saulolimajf@gmail.com>
2014-04-12 15:55:32 -0300

    Ajuste do menu da home

--- app/views/menu/main.php
+++ app/views/menu/main.php
@@ -1,3 +1,10 @@
 <?php
-
-echo Navbar::create()->with_menus(Navigation::links(Category::menu()), array(), true, true);
+#echo Navbar::create()->with_menus(Navigation::links(Category::menu()), array(), true, true);
+?>
+<ul class="navbar-nav nav">
+    <li><a href="hoteis-e-pousadas">Hot&eacute;is &amp; Pousadas</a></li>
+    <li><a href="pacotes-nacionais">Pacotes Nacionais</a></li>
+    <li><a href="pacotes-internacionais">Pacotes Internacionais</a></li>
+    <li><a href="feriados" class="single-line"><span>Feriados</span></a></li>
+    <li><a href="passeios">Passeios &amp; Gatronomia</a></li>
+</ul>
