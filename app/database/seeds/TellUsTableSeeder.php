8f909ade75e7fcacf812d73cccd600ea5d527866
Cawe Coy <cawecoy@gmail.com>
2014-04-14 11:24:56 -0300

    Seed com host, database, username e password puxado de Config

--- app/database/seeds/TellUsTableSeeder.php
+++ app/database/seeds/TellUsTableSeeder.php
@@ -10,7 +10,7 @@
 //   {
 //     public function run()
 //   {
-//     $pdo = new PDO('mysql:host=innbativel.ctypvw54pblq.us-east-1.rds.amazonaws.com;dbname=innbativel', 'innbativel', 'aWdh2kHAF6A3');
+//     $pdo = new PDO('mysql:host='.Config::get('database.connections.mysql.host').';dbname='.Config::get('database.connections.mysql.database'), Config::get('database.connections.mysql.username'), Config::get('database.connections.mysql.password'));
 
 //     $config = new LexerConfig();
 //     $config->setToCharset('UTF-8');
