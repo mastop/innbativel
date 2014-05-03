9330eecfef32801c9c6349b6cdf12929d908847a
salima <saulolimajf@gmail.com>
2014-05-03 13:15:58 -0300

    ajustes de layout na home

--- app/views/themes/floripa/frontend/html.blade.php
+++ app/views/themes/floripa/frontend/html.blade.php
@@ -83,7 +83,6 @@
         </div>
     </div>
 
-
     <div id="header" class="navbar navbar-default navbar-fixed-top">
         <div class="topbar">
             <div class="container">
@@ -123,19 +122,10 @@
 
 	<nav id="nav">
 		<div class="container">
-			<nav class="navbar-default navbar" role="navigation">
-				<div class="container">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navMenuCollapse">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					</div>
-					<div class="collapse navbar-collapse navMenuCollapse">
-                        @include('menu.main')
-					</div>
-				</div>
+			<nav class="collapse navbar-collapse navMenuCollapse" role="navigation">
+                <ul class="navbar-nav nav">
+                    @include('menu.main')
+                </ul>
 			</nav>
 		</div>
 	</nav>
@@ -146,69 +136,11 @@
 
 	<div id="pre-footer" class="clearfix">
 		<div class="container">
-			<div id="widget-selos">
-				<h4><span class="entypo lock"></span>Compre com Segurança</h4>
-				<div id="selo-bandeiras">
-					<figure><img src="{{ asset('assets/images/bandeiras.png') }}" alt="Bandeiras de Cartão de Crédito" width="212" height="83"></figure>
-				</div>
-				<div id="selo-ebit">
-					<figure>
-						<a id="seloEbit" href="http://www.ebit.com.br/#innbativel" target="_blank" onclick="redir(this.href);">Avaliação de Lojas e-bit</a>
-						<script type="text/javascript" id="getSelo" src="https://558701205.r.anankecdn.com.br/ebitBR/static/getSelo.js?40747" >
-						</script>
-					</figure>
-				</div>
-				<div id="selo-siteblindado">
-					<a href="https://selo.siteblindado.com.br/verificar?url=innbativel.com.br" title="Visualizou o selo Site Blindado? Navegue tranquilamente, esse site está BLINDADO CONTRA ATAQUES. Realizamos milhares de testes simulando ataques de hacker, para garantir a segurança do site. Clique no selo e confira o certificado."><img src="https://s3-sa-east-1.amazonaws.com/selo.siteblindado.com/seals_aw/innbativel.com.br/siteblindado_pr.gif" alt="Selo Site Blindado"></a>
-					<param id="aw_nav_post" value="1">
-					<script type="text/javascript" src="//selo.siteblindado.com/aw.js"></script>
-				</div>
-				<div id="selo-embratur">
-					<figure>
-						<img src="{{ asset('assets/images/embratur.png') }}" alt="Embratur" width="140" height="19">
-					</figure>
-				</div>
-				<div id="selo-site-sustentavel">
-					<a href="http://www.sitesustentavel.com.br/certificado/list?url=innbativel.com.br" title="Site Sustentável" target="_blank">
-						<img src="{{ asset('assets/images/site-sustentavel.png') }}" alt="Site Sustentável" width="142" height="40">
-					</a>
-				</div>
-			</div>
-
-			<div id="widget-facebook">
-				<h4>INNBatível no Facebook</h4>
-				<div class="fb-like-box" data-href="http://www.facebook.com/innbativel" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false" width="292"></div>
-
-				<div id="fb-root"></div>
-				<script>
-					(function(d, s, id) {
-					  var js, fjs = d.getElementsByTagName(s)[0];
-					  if (d.getElementById(id)) return;
-					  js = d.createElement(s); js.id = id;
-					  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
-					  fjs.parentNode.insertBefore(js, fjs);
-					}(document, 'script', 'facebook-jssdk'));
-				</script>
-
-			</div>
-			<div id="widget-twitter">
-				<h4>Twitter do INNBatível</h4>
-				<a class="twitter-timeline" width="300" height="230" href="https://twitter.com/INNBativel" data-widget-id="279588524427190272">Tweets de @INNBativel</a>
-
-				<script>
-					try {!function(d,s,id){
-						var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
-						if(!d.getElementById(id)){js=d.createElement(s);
-							js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
-							fjs.parentNode.insertBefore(js,fjs);
-						}
-					}(document,"script","twitter-wjs");} catch(e){}
-				</script>
-
-			</div>
+            @include('partials.widget-selos')
+            @include('partials.widget-facebook')
+            @include('partials.widget-twitter')
 		</div>
 	</div>
-
 
     @include('partials.parceiro')
     @include('partials.conte-pra-gente')
