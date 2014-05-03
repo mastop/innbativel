8a24a2c8ce42f6c9cf6ee8bc049a0077427c02ff
salima <saulolimajf@gmail.com>
2014-05-03 12:30:48 -0300

    criação de partials para ações de modal na home

---
+++ app/views/partials/quem-somos.blade.php
@@ -0,0 +1,41 @@
+<div id="quem-somos" class="modal fade" tabindex="-1">
+    <div class="modal-dialog modal-sm modal-combo-">
+        <div class="modal-content">
+            <div class="modal-header">
+                <a href="{{ route('home') }}">
+                    <img class="logo" alt="INNbatível" src="{{ asset('assets/images/logo.png') }}">
+                </a>
+            </div>
+            <div class="modal-body">
+                <h4 class="modal-title">Quem somos</h4>
+                <br>
+                <p>
+                    O INNBatível é o primeiro site brasileiro de compras coletivas 2.0 especializado em Turismo.<br>
+                    Em pouco mais de três anos de empresa, já embarcamos milhares de clientes para os mais diversos destinos nacionais e internacionais.
+                </p>
+                <p>
+                    Nosso maior objetivo é proporcionar sempre as melhores ofertas para que você possa realizar aquela tão sonhada viagem. São descontos de 50% a 80% em pousadas de luxo, hotéis, resorts, cruzeiros marítimos, passagens aéreas, entretenimento, passeios e muito mais.<br>
+                    Nós do INNBatível nos orgulhamos em oferecer somente os melhores estabelecimentos. Sempre nos preocupamos com a qualidade dos serviços ofertados e principalmente com a capacidade de atendimento.
+                </p>
+                <p>
+                    Você sabe qual o significado do nome INNBatível?
+                </p>
+                <p class="text-center">
+                    <strong>INN</strong><br>(sinônimo de hospedagem)<br>
+                    +<br>
+                    <strong>Ofertas com descontos IMBATÍVEIS</strong><br>
+                    =<br>
+                    <strong>INNBatível</strong>
+                </p>
+                <p>
+                    Nós somos assim:<br>
+                    Você e seus sonhos em primeiro lugar,<br>
+                    para que sempre <strong>viaje mais por menos!</strong>
+                </p>
+                <p>
+                    Grupo INNBatível
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
