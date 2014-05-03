fdcd79f45696f72efd0a4ce1ce33f39754165d3c
salima <saulolimajf@gmail.com>
2014-05-03 13:29:28 -0300

    partials para widget

---
+++ app/views/partials/widget/twitter.blade.php
@@ -0,0 +1,14 @@
+<div id="widget-twitter">
+    <h4>Twitter do INNBatível</h4>
+    <a class="twitter-timeline" width="300" height="230" href="https://twitter.com/INNBativel" data-widget-id="279588524427190272">Tweets de @INNBativel</a>
+
+    <script>
+        try {!function(d,s,id){
+            var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+            if(!d.getElementById(id)){js=d.createElement(s);
+                js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+                fjs.parentNode.insertBefore(js,fjs);
+            }
+        }(document,"script","twitter-wjs");} catch(e){}
+    </script>
+</div>
