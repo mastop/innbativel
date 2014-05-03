d3947b3cb12896f488698f65097c89a0ca138395
salima <saulolimajf@gmail.com>
2014-05-03 16:37:56 -0300

    criação de templates para o envio de email no fale conosco

---
+++ app/views/emails/contact/reply.blade.php
@@ -0,0 +1,8 @@
+<p>
+  Ol&aacute; {{ $name }},<br /><br />
+  Recebemos seu contato e agradecemos pelo interesse.
+</p>
+<p>
+&nbsp;<br />
+<br />
+Equipe InnBat&iacute;vel</p>
---
+++ app/views/emails/contact/send.blade.php
@@ -0,0 +1,12 @@
+<h1>FALE CONOSCO</h1>
+<p>
+  Ol&aacute; Equipe InnBat&iacute;vel, <br /><br />
+  O  {{ $name }}, entrou em contato pelo fale conosco do site, com os seguintes dados:
+<hr>
+<p>
+    <b>Nome:</b>{{ $name }}<br />
+    <b>E-mail:</b>{{ $email }}<br />
+    <b>Telefone:</b>{{ $telefone }}<br />
+    <b>Celular:</b>{{ $celuar }}<br />
+    <p>{{ $msg }}</p>
+</p>
