9dcb5cade26f8aeb5d3179e52ea3db7418213895
fernanDOTdo <o@fernan.do>
2014-05-28 15:43:29 -0300

    Correção dos Triggers do Cawe

--- app/database/seeds/Triggers.php
+++ app/database/seeds/Triggers.php
@@ -4,51 +4,45 @@ class Triggers extends DatabaseSeeder
 {
 	public function run()
 	{
-	    $DB_NAME = Config::get('database.connections.mysql.database');
-		$DB_HOST = Config::get('database.connections.mysql.host');
-		$DB_USER = Config::get('database.connections.mysql.username'); 
-		$DB_PASS = Config::get('database.connections.mysql.password'); 
-
-		$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-
-
-
+        $this->command->info('Início dos Triggers');
+        $pdo = DB::connection()->getPdo();
 		$triggers = 'DROP TRIGGER IF EXISTS inst_order;';
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+
+        if($pdo->exec($triggers) === false) print_r($pdo->errorInfo());
 
 		$triggers = file_get_contents(app_path().'/database/seeds/assets/Triggers/inst_order.sql');
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+		($pdo->exec($triggers) === false) ? print_r($pdo->errorInfo()) : $this->command->info('Trigger inst_order criado.');
 
 
 
 		$triggers = 'DROP TRIGGER IF EXISTS upd_order;';
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+		if($pdo->exec($triggers) === false) print_r($pdo->errorInfo());
 
 		$triggers = file_get_contents(app_path().'/database/seeds/assets/Triggers/upd_order.sql');
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+        ($pdo->exec($triggers) === false) ? print_r($pdo->errorInfo()) : $this->command->info('Trigger upd_order criado.');
 
 
 
 		$triggers = 'DROP FUNCTION IF EXISTS inst_transaction_partial_cancellation;';
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+		if($pdo->exec($triggers) === false) print_r($pdo->errorInfo());
 
 		$triggers = file_get_contents(app_path().'/database/seeds/assets/Triggers/inst_transaction_partial_cancellation.sql');
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+        ($pdo->exec($triggers) === false) ? print_r($pdo->errorInfo()) : $this->command->info('Trigger inst_transaction_partial_cancellation criado.');
 
 
 
 		$triggers = 'DROP TRIGGER IF EXISTS inst_voucher;';
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+		if($pdo->exec($triggers) === false) print_r($pdo->errorInfo());
 
 		$triggers = file_get_contents(app_path().'/database/seeds/assets/Triggers/inst_voucher.sql');
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+        ($pdo->exec($triggers) === false) ? print_r($pdo->errorInfo()) : $this->command->info('Trigger inst_voucher criado.');
 
 
 
 		$triggers = 'DROP TRIGGER IF EXISTS upd_voucher;';
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+		if($pdo->exec($triggers) === false) print_r($pdo->errorInfo());
 
 		$triggers = file_get_contents(app_path().'/database/seeds/assets/Triggers/upd_voucher.sql');
-		if(!$mysqli->query($triggers)) print_r($mysqli->error);
+        ($pdo->exec($triggers) === false) ? print_r($pdo->errorInfo()) : $this->command->info('Trigger upd_voucher criado.');
 	}
 }
