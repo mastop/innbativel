45451f97cf147a778e5f314c78777e869ae6f137
fernanDOTdo <o@fernan.do>
2014-06-23 10:21:23 -0300

    Correção dos links dos menus

---
+++ app/views/menu/footer.php
@@ -0,0 +1,29 @@
+<div id="menu-footer">
+    <ul id="menu-viaje">
+        <li><strong>Viaje</strong></li>
+        <li><a href="{{url('hoteis-e-pousadas')}}">Hot&eacute;is &amp; Pousadas</a></li>
+        <li><a href="{{url('pacotes-nacionais')}}">Pacotes Nacionais</a></li>
+        <li><a href="{{url('pacotes-internacionais')}}">Pacotes Internacionais</a></li>
+        <li><a href="{{url('feriados')}}">Feriados</a></li>
+        <li><a href="{{url('passeios-gastronomia')}}">Passeios &amp; Gatronomia</a></li>
+    </ul>
+    <ul id="menu-participe">
+        <li><strong>Participe</strong></li>
+        <li><a href="#faq" data-toggle="modal">Perguntas Frequentes</a></li>
+        <li><a href="#parceiro" data-toggle="modal">Seja Nosso Parceiro</a></li>
+        <li><a href="#sugira" data-toggle="modal">Sugira uma Viagem</a></li>
+        <li><a href="#conte-pra-gente" data-toggle="modal">Conte pra Gente</a></li>
+        <!-- <li><a href="#">Indique e Ganhe</a></li> -->
+    </ul>
+    <ul id="menu-institucional">
+        <li><strong>Institucional</strong></li>
+        <!-- <li><a href="#faq" data-toggle="modal">Ajuda</a></li> -->
+        <li><a href="#contact" data-toggle="modal">Fale Conosco</a></li>
+        <li><a href="#quem-somos" data-toggle="modal">Quem Somos</a></li>
+        <li><a href="#acao-social" data-toggle="modal">Ação Social</a></li>
+        <li><a href="#trabalhe-conosco" data-toggle="modal">Trabalhe Conosco</a></li>
+        <li><a href="#press" data-toggle="modal">Imprensa</a></li>
+        <li><a href="#termos" data-toggle="modal">Termos e Condições de Uso</a></li>
+        <li><a href="#politica" data-toggle="modal">Política de Privacidade</a></li>
+    </ul>
+</div>
---
+++ app/views/menu/main.blade.php
@@ -0,0 +1,7 @@
+<ul class="navbar-nav nav">
+    <li><a href="{{url('hoteis-e-pousadas')}}">Hot&eacute;is &amp; Pousadas</a></li>
+    <li><a href="{{url('pacotes-nacionais')}}">Pacotes Nacionais</a></li>
+    <li><a href="{{url('pacotes-internacionais')}}">Pacotes Internacionais</a></li>
+    <li><a href="{{url('feriados')}}" class="single-line"><span>Feriados</span></a></li>
+    <li><a href="{{url('passeios')}}">Passeios &amp; Gatronomia</a></li>
+</ul>
