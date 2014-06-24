96f9d671f952e667d673c550f319dc8b4dabe9fe
Cawe Coy <cawecoy@gmail.com>
2014-06-24 20:11:13 -0300

    Otimização na Migração de users

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -22,20 +22,10 @@ class UsersTableSeeder extends DatabaseSeeder
 		try{
 			$columns[6] = $columns[5] = Str::slug(substr($columns[2], 0, 10)) . '-' . $columns[0];
 			
-			// $stmt = $pdo->prepare('INSERT INTO users (id, salt, email, password, created_at, username, api_key) VALUES (?, ?, ?, ?, ?, ?, ?)');
-	  //   	if(!$stmt->execute($columns)){ print_r($columns); print_r($stmt->errorInfo()); }
+			$stmt = $pdo->prepare('INSERT INTO users (id, salt, email, password, created_at, username, api_key) VALUES (?, ?, ?, ?, ?, ?, ?)');
+	    	if(!$stmt->execute($columns)){ print_r($columns); print_r($stmt->errorInfo()); }
 	    	
-	    	$user = [
-	    		'id' => $columns[0],
-	    		'salt' => $columns[1],
-	    		'email' => $columns[2],
-	    		'password' => $columns[3],
-	    		'created_at' => $columns[4],
-	    		'username' => $columns[5],
-	    		'api_key' => $columns[6],
-	    	];
-
-	    	User::create($user);
+	    	User::where('id', $columns[0])->update(['password' => $columns[3]]);
 		}
 		catch (Exception $e) {
 			print_r($columns);
