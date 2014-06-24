efa5c5118476eb0fe0f21b9f9569f3b6d8f568d1
Cawe Coy <cawecoy@gmail.com>
2014-06-24 20:16:55 -0300

    hash no password da Migração de users

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -21,11 +21,10 @@ class UsersTableSeeder extends DatabaseSeeder
 	$interpreter->addObserver(function(array $columns) use ($pdo) {
 		try{
 			$columns[6] = $columns[5] = Str::slug(substr($columns[2], 0, 10)) . '-' . $columns[0];
+			$columns[3] = Hash::make($columns[3]);
 			
 			$stmt = $pdo->prepare('INSERT INTO users (id, salt, email, password, created_at, username, api_key) VALUES (?, ?, ?, ?, ?, ?, ?)');
 	    	if(!$stmt->execute($columns)){ print_r($columns); print_r($stmt->errorInfo()); }
-	    	
-	    	User::where('id', $columns[0])->update(['password' => $columns[3]]);
 		}
 		catch (Exception $e) {
 			print_r($columns);
