da8379f28e54022307c5d07f2bc69e05c86ec87a
Cawe Coy <cawecoy@gmail.com>
2014-06-25 17:16:07 -0300

    Migração de usuarios - atualização

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -18,24 +18,21 @@ class UsersTableSeeder extends DatabaseSeeder
 	$interpreter = new Interpreter();
 	$interpreter->unstrict();
 
+	ini_set('memory_limit', '2500M');
+
 	$interpreter->addObserver(function(array $columns) use ($pdo) {
 		try{
-			$columns[6] = $columns[5] = Str::slug(substr($columns[2], 0, 10)) . '-' . $columns[0];
-			
 			// $stmt = $pdo->prepare('INSERT INTO users (id, salt, email, password, created_at, username, api_key) VALUES (?, ?, ?, ?, ?, ?, ?)');
 	  //   	if(!$stmt->execute($columns)){ print_r($columns); print_r($stmt->errorInfo()); }
-	    	
-	    	$user = [
-	    		'id' => $columns[0],
-	    		'salt' => $columns[1],
-	    		'email' => $columns[2],
-	    		'password' => $columns[3],
-	    		'created_at' => $columns[4],
-	    		'username' => $columns[5],
-	    		'api_key' => $columns[6],
-	    	];
-
-	    	User::create($user);
+	    	$user = new User;
+	    	$user->id = $columns[0];
+	    	$user->username = $columns[0];
+	    	$user->api_key = $columns[0];
+	    	$user->salt = $columns[1];
+	    	$user->email = $columns[2];
+	    	$user->password = $columns[3];
+	    	$user->created_at = $columns[4];
+	    	$user->save();
 		}
 		catch (Exception $e) {
 			print_r($columns);
