7f596b7dffe549e509b8c697effa5454790f369d
Cawe Coy <cawecoy@gmail.com>
2014-06-30 06:10:30 -0300

    Termos de uso e Política de privacidade carregados a partir de Configuration::get()

--- app/views/partials/modal/politica.blade.php
+++ app/views/partials/modal/politica.blade.php
@@ -6,52 +6,7 @@
             </div>
             <div class="modal-body">
                 <h4>​Política de privacidade do INNBatível​</h4>​
-                <p>
-                    Nosso compromisso é respeitar sua privacidade e garantir o sigilo de todas as informações que você nos fornece. Todos os dados cadastrados em nosso site são utilizados apenas para melhorar sua experiência de compra e mantê-lo atualizado sobre nossas promoções e vantagens oferecidas por empresas parceiras do INNBatível.</p>
-                <p>
-                    <em>Uso e Segurança das informações</em></p>
-                <p>
-                    Para seu cadastro, solicitamos informações como nome, endereço, e-mail e telefones para contato, para facilitar suas compras no site.</p>
-                <p>
-                    O seu e-mail é utilizado para divulgar informações de suas compras e, quando solicitado por você, para comunicar promoções de produtos e serviços do INNBatível e suas parceiras.</p>
-                <p>
-                    O INNBatível somente compartilha as informações pessoais com empresas afiliadas do INNBatível e exclusivamente com o objetivo de fornecer a você as melhores ofertas de serviços, ou em circunstâncias específicas como ordem judicial ou advindas de lei.</p>
-                <p>
-                    Tomamos medidas de segurança adequadas para nos proteger contra acesso, alteração, divulgação ou destruição não autorizada dos dados. Essas medidas incluem análises internas de nossas práticas de coleta, armazenamento e processamento de dados e medidas de segurança, incluindo criptografia e medidas de segurança físicas apropriadas para nos proteger contra o acesso não autorizado a sistemas em que armazenamos os dados pessoais.</p>
-                <p>
-                    Limitamos o acesso às informações pessoais aos funcionários, contratantes e agentes do INNBatível que precisam ter conhecimento dessas informações para processá-las em nosso nome. Essas pessoas estão comprometidas com as obrigações de confidencialidade e podem ser submetidas a punições, incluindo rescisão de contrato e processo criminal, caso não cumpram essas obrigações.</p>
-                <p>
-                    <em>Cookies</em></p>
-                <p>
-                    No innbativel.com.br, o uso de cookies é feito apenas para reconhecer um visitante constante e melhorar a experiência de compra. Os cookies são pequenos arquivos de dados transferidos de um site da web para o disco do seu computador e não armazenam dados pessoais. Se preferir, você pode apagar os cookies existentes em seu computador através do browser.</p>
-                <p>
-                    <em>Envio de e-mails</em></p>
-                <p>
-                    O innbativel.com.br nunca envia e-mails solicitando confirmação de dados/cadastro ou com anexos executáveis (extensão .exe, .com, .scr, .bat) e links para download.</p>
-                <p>
-                    Nossos e-mails com promoções têm como remetentes: news@innbativelnews.com.br e contato@innbativel.com.br. Os e-mails de acompanhamento de seu pedido são: atendimento@innbativel.com.br e faleconosco@innbativel.com.br</p>
-                <p>
-                    Os links de nossos e-mails levam diretamente para o innbativel.com.br ou para empresas parceiras.</p>
-                <p>
-                    Nunca forneça a senha de seu cadastro a terceiros e, no caso de uso não autorizado, acesse imediatamente a área “Meus Dados” em “Minha Conta” no site e altere sua senha.</p>
-                <p>
-                    Caso deseje parar de receber nossos e-mails, basta clicar no link “remova aqui” que se localiza no rodapé de todos os e-mails enviados.</p>
-                <p>
-                    Em caso de dúvidas ou sugestões, por favor, entre em contato conosco pelo e-mail faleconosco@innbativel.com.br</p>
-                <p>
-                    Alterações nesta Política de Privacidade</p>
-                <p>
-                    Esta Política de Privacidade pode ser alterada periodicamente.<br>
-                    <br>
-                    Esta Política de Privacidade não reduzirá os seus direitos sem o seu consentimento explícito. Publicaremos todas as alterações da Política de Privacidade nesta página e, se as alterações forem significativas, colocaremos um aviso com mais destaque, incluindo, para alguns serviços, notificação por e-mail das alterações da Política de Privacidade. Também manteremos as versões anteriores desta Política de Privacidade arquivadas para você visualizá-las.</p>
-                <p>
-                    Compra segura no innbativel.com.br</p>
-                <p>
-                    Nosso site utiliza uma tecnologia avançada de segurança. Todo o tráfego de dados é feito com as informações criptografadas, utilizando-se do certificado SSL (Secure Socket Layer), que é um método padrão usado na Internet para proteger as comunicações entre os usuários da Web e os sites, fornecendo uma compra 100% segura.</p>
-                <p>
-                    As informações de cartões de crédito não são armazenadas em nossos sistemas e todo o processo de aprovação é feito diretamente com as Administradoras de Cartões e Bancos. Para sua tranquilidade, o ícone do cadeado fechado, no canto superior da tela, indica absoluta segurança durante a transmissão de dados em sua compra.</p>
-                <p>
-                    Para sua segurança, se houver qualquer divergência de informações cadastrais e de pagamento, o innbativel.com.br entrará em contato para confirmar os dados antes de aprovar o pedido.</p>
+                {{ Configuration::get('privacy_policy') }}
             </div>
         </div>
     </div>
