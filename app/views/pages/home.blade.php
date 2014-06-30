a2861b089f720543804779a909ae0ffc491cf68f
fernanDOTdo <o@fernan.do>
2014-06-30 06:08:18 -0300

    Home Atualizada

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -48,14 +48,14 @@
         @if($k == 0) @continue @endif
 		<div class="offer-grid row offer-group">
 
-		  <h3>{{ $group->title }}</h3>
+		  <h3><span class="entypo {{ $group->icon }}"></span> {{ $group->title }}</h3>
 
 		  @foreach ($group->offer as $offer)
 		  <div itemscope class="offer-grid-item col-6 col-sm-6 col-lg-6 clearfix">
               @include('partials.oferta', array('offer'=>$offer))
 		  </div>
 		  @endforeach
-		  <a class="more" href="{{ $group->url }}">Veja mais ofertas</a>
+		  <a class="more" href="{{ $group->url }}">Veja mais ofertas de <strong>{{ $group->short_title }}</strong></a>
 
 		</div>
 		@endforeach
