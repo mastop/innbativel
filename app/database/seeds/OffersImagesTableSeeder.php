712427458208784541f985db3d9fd44011adbad3
Cawe Coy <cawecoy@gmail.com>
2014-07-01 22:26:43 -0300

    Seeder para migração parte 2

--- app/database/seeds/OffersImagesTableSeeder.php
+++ app/database/seeds/OffersImagesTableSeeder.php
@@ -1,55 +1,55 @@
 <?php
-// use Goodby\CSV\Import\Standard\Lexer;
-// use Goodby\CSV\Import\Standard\Interpreter;
-// use Goodby\CSV\Import\Standard\LexerConfig;
-
-// class OffersImagesTableSeeder extends DatabaseSeeder
-// {
-//   public function run()
-//   {
-//     $pdo = new PDO('mysql:host='.Config::get('database.connections.mysql.host').';dbname='.Config::get('database.connections.mysql.database'), Config::get('database.connections.mysql.username'), Config::get('database.connections.mysql.password'));
-
-//     $config = new LexerConfig();
-//     $config->setFromCharset('UTF-8');
-//     $config->setToCharset('LATIN1');
-//     $lexer = new Lexer($config); //ISO8591 ou UTF-8
-
-//     $interpreter = new Interpreter();
-//     $interpreter->unstrict();
-
-//     $interpreter->addObserver(function(array $columns) use ($pdo) {
-//         try{
-//             $stmt = $pdo->prepare('INSERT INTO offers_images (offer_id, url) VALUES (?, ?)');
-//             if(!$stmt->execute($columns)){ print_r($columns); print_r($stmt->errorInfo()); }
-//         }
-//         catch (Exception $e) {
-//             print_r($columns);
-//             print_r($e->getMessage()."\n");
-//         }
-//     });
-
-//     $lexer->parse(app_path().'/database/seeds/assets/ImportacaoBDNovo/offers_images.csv', $interpreter);
-//   }
-// }
+use Goodby\CSV\Import\Standard\Lexer;
+use Goodby\CSV\Import\Standard\Interpreter;
+use Goodby\CSV\Import\Standard\LexerConfig;
 
 class OffersImagesTableSeeder extends DatabaseSeeder
 {
   public function run()
   {
-	// $offers_images = [
-	//   [
-	//     'offer_id' => 783,
-	//     'url' => 'http://ofertasinnbativel.s3.amazonaws.com/c683df579fcdeb8334ffbabdeaa6940e2270_t.jpg',
-	//   ],
-	//   [
-	//     'offer_id' => 784,
-	//     'url' => 'http://ofertasinnbativel.s3.amazonaws.com/08ee15aa60a7b2cd6352ab38e073eea712336_t.jpg',
-	//   ],
-	// ];
+    $pdo = new PDO('mysql:host='.Config::get('database.connections.mysql.host').';dbname='.Config::get('database.connections.mysql.database'), Config::get('database.connections.mysql.username'), Config::get('database.connections.mysql.password'));
 
-	// foreach ($offers_images as $offer_image)
-	// {
-	//   OfferImage::create($offer_image);
-	// }
+    $config = new LexerConfig();
+    $config->setFromCharset('UTF-8');
+    $config->setToCharset('LATIN1');
+    $lexer = new Lexer($config); //ISO8591 ou UTF-8
+
+    $interpreter = new Interpreter();
+    $interpreter->unstrict();
+
+    $interpreter->addObserver(function(array $columns) use ($pdo) {
+        try{
+            $stmt = $pdo->prepare('INSERT INTO offers_images (offer_id, url) VALUES (?, ?)');
+            if(!$stmt->execute($columns)){ print_r($columns); print_r($stmt->errorInfo()); }
+        }
+        catch (Exception $e) {
+            print_r($columns);
+            print_r($e->getMessage()."\n");
+        }
+    });
+
+    $lexer->parse(app_path().'/database/seeds/assets/ImportacaoBDNovo/offers_images.csv', $interpreter);
   }
 }
+
+// class OffersImagesTableSeeder extends DatabaseSeeder
+// {
+//   public function run()
+//   {
+// 	// $offers_images = [
+// 	//   [
+// 	//     'offer_id' => 783,
+// 	//     'url' => 'http://ofertasinnbativel.s3.amazonaws.com/c683df579fcdeb8334ffbabdeaa6940e2270_t.jpg',
+// 	//   ],
+// 	//   [
+// 	//     'offer_id' => 784,
+// 	//     'url' => 'http://ofertasinnbativel.s3.amazonaws.com/08ee15aa60a7b2cd6352ab38e073eea712336_t.jpg',
+// 	//   ],
+// 	// ];
+
+// 	// foreach ($offers_images as $offer_image)
+// 	// {
+// 	//   OfferImage::create($offer_image);
+// 	// }
+//   }
+// }
