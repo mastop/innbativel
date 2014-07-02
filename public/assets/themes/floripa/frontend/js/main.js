672ee9bc975b02ee8157212c7dd130fd14f5c090
fernanDOTdo <o@fernan.do>
2014-07-02 06:47:31 -0300

    Alteração no Facebook.
    Tentativa de pegar o APP ID

--- public/assets/themes/floripa/frontend/js/main.js
+++ public/assets/themes/floripa/frontend/js/main.js
@@ -6,14 +6,6 @@
 		
 		Innbativel.load = function(){
 
-			(function(d, s, id) {
-				var js, fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
-				fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-
 			try {!function(d,s,id){
 				var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
 				if(!d.getElementById(id)){js=d.createElement(s);
