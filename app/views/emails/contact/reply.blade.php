b2fd55e399982c5ce91b4a3291eea3dd17e6d94e
Cawe Coy <cawecoy@gmail.com>
2014-07-10 17:16:30 -0300

    Ajuste nos e-mails transacionais

--- app/views/emails/contact/reply.blade.php
+++ app/views/emails/contact/reply.blade.php
@@ -1,3 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>INNBatível - Viaje mais por menos</title>
+</head>
+<body>
 <p>
   Ol&aacute; {{ $name }},<br /><br />
   Recebemos seu contato e agradecemos pelo interesse.
@@ -6,3 +13,5 @@
 &nbsp;<br />
 <br />
 Equipe InnBat&iacute;vel</p>
+</body>
+</html>
--- app/views/emails/suggest/create.blade.php
+++ app/views/emails/suggest/create.blade.php
@@ -1,3 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>INNBatível - Viaje mais por menos</title>
+</head>
+<body>
 <h1>SUGIRA UMA VIAGEM</h1>
 <p>
     Ol&aacute; Equipe InnBat&iacute;vel, <br /><br />
@@ -10,3 +17,5 @@
     <b>Sugest&atilde;o:</b>{{ $suggestion }}<br />
 
 </p>
+</body>
+</html>
