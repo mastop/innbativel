8dbf383576d3e178b1c80db77b04a17c2f8a6c05
fernanDOTdo <o@fernan.do>
2014-07-10 23:57:20 -0300

    Permitir que a home fique sem banners nem destaque principal

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,48 +1,49 @@
 @section('content')
 	<div id="main" class="container">
+        @if(isset($groups[0]->offer[0]) || count($banners) > 0)
+            <div class="row">
+              @if(isset($groups[0]->offer[0]))
+              <div itemscope class="offer-spotlight offer-grid-item col-7 col-sm-7 col-lg-7 clearfix">
+                  @include('partials.oferta', array('offer'=>$groups[0]->offer[0]))
+              </div>
+              @endif
+              <div class="col-5 col-sm-5 col-lg-5">
 
-		<div class="row">
-          @if(isset($groups[0]->offer[0]))
-		  <div itemscope class="offer-spotlight offer-grid-item col-7 col-sm-7 col-lg-7 clearfix">
-              @include('partials.oferta', array('offer'=>$groups[0]->offer[0]))
-		  </div>
-          @endif
-		  <div class="col-5 col-sm-5 col-lg-5">
+                <div id="banner-carousel" class="carousel slide banner-grid-item clearfix" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        @foreach($banners as $k => $banner)
+                        <li data-target="#banner-carousel" data-slide-to="{{$k}}" @if($k == 0) class="active" @endif></li>
+                        @endforeach
+                    </ol>
 
-		    <div id="banner-carousel" class="carousel slide banner-grid-item clearfix" data-ride="carousel">
-		        <!-- Indicators -->
-		        <ol class="carousel-indicators">
-                    @foreach($banners as $k => $banner)
-                    <li data-target="#banner-carousel" data-slide-to="{{$k}}" @if($k == 0) class="active" @endif></li>
-                    @endforeach
-		        </ol>
-
-		        <!-- Wrapper for slides -->
-		        <div class="carousel-inner">
-                    @foreach($banners as $k => $banner)
-                    <div class="item @if($k == 0) active @endif">
-                        <a href="{{ $banner->url }}" class="banner-grid-inner clearfix">
-                            <img src="{{ $banner->img }}" alt="{{ $banner->title }}">
-                            @if($banner->title)
-                                <header @if($banner->subtitle) class="tooltip" data-tip="{{ $banner->subtitle }}@endif">
-                                    <h2>{{ $banner->title }}</h2>
-                                </header>
-                            @endif
-                        </a>
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        @foreach($banners as $k => $banner)
+                        <div class="item @if($k == 0) active @endif">
+                            <a href="{{ $banner->url }}" class="banner-grid-inner clearfix">
+                                <img src="{{ $banner->img }}" alt="{{ $banner->title }}">
+                                @if($banner->title)
+                                    <header @if($banner->subtitle) class="tooltip" data-tip="{{ $banner->subtitle }}@endif">
+                                        <h2>{{ $banner->title }}</h2>
+                                    </header>
+                                @endif
+                            </a>
+                        </div>
+                        @endforeach
                     </div>
-                    @endforeach
-		        </div>
 
-		        <!-- Controls -->
-		        <a class="left carousel-control" href="#banner-carousel" data-slide="prev">
-		            <span class="glyphicon glyphicon-chevron-left"></span>
-		        </a>
-		        <a class="right carousel-control" href="#banner-carousel" data-slide="next">
-		            <span class="glyphicon glyphicon-chevron-right"></span>
-		        </a>
-		    </div>
-		  </div>
-		</div>
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#banner-carousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a class="right carousel-control" href="#banner-carousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                </div>
+              </div>
+            </div>
+        @endif
 
 		@foreach ($groups as $k => $group)
         @if($k == 0) @continue @endif
