ed5d1e6ecb656e8fe6153d852600376f2c0b818a
fernanDOTdo <o@fernan.do>
2014-07-10 23:49:23 -0300

    Removendo temporariamente o link de "Passeios & Gastronomia"

--- app/views/menu/main.blade.php
+++ app/views/menu/main.blade.php
@@ -3,5 +3,5 @@
     <li><a href="{{url('pacotes-nacionais')}}">Pacotes Nacionais</a></li>
     <li><a href="{{url('pacotes-internacionais')}}">Pacotes Internacionais</a></li>
     <li><a href="{{url('feriados')}}" class="single-line"><span>Feriados</span></a></li>
-    <li><a href="{{url('passeios')}}">Passeios &amp; Gatronomia</a></li>
+    <li><a href="{{-- url('passeios') --}}#">Passeios &amp; Gatronomia  <span class="badge" style="background-color: #999; font-size:0.8em; font-weight: 400; position: absolute; right: 2px; top: 28px;">Breve</span></a></li>
 </ul>
